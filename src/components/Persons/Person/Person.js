cd286f182c43c7a4e64725f201d0ca9e06179d6a
mriduladas <mridula199@gmail.com>
2021-02-17 10:26:59 +0530

    Returned multiple Javascripts tags as array in Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,11 +11,11 @@ const person = (props) => {
     // }
     console.log('Person.js render')
         return (
-            <div className={classes.Person} >
-            <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange = {props.change} value={props.name} />
-            </div>
+            // <div className={classes.Person} >
+            [<p key="i1" onClick={props.click}>I am {props.name} and I am {props.age} years old</p>,
+            <p key="i2" >{props.children}</p>,
+            <input key="i3" type="text" onChange = {props.change} value={props.name} />]
+            // </div>
         );
 
 
